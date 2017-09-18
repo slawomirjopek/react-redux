d0a0d4edcf098c6e38b26faa6cfbd47a434bb800
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-18 20:01:34 +0200

    redux basics

---
+++ src/app/index.js
@@ -0,0 +1,32 @@
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+    if (action.type === 'INC') {
+        return state + (checkPayload(action.payload) ? action.payload : 1);
+    }
+
+    if (action.type === 'DEC') {
+        return state - (checkPayload(action.payload) ? action.payload : 1);
+    }
+
+    return state;
+};
+
+window.store = createStore(reducer, 0);
+
+store.subscribe(() => {
+    console.log('store changed...', store.getState());
+});
+
+function checkPayload(payload) {
+    return typeof payload === 'number';
+}
+
+store.dispatch({ type: 'INC'}); // 1
+store.dispatch({ type: 'DEC'}); // 0
+
+store.dispatch({ type: 'INC', payload: 10}); // 10
+store.dispatch({ type: 'DEC', payload: 5}); // 5
+
+store.dispatch({ type: 'INC', payload: 'not number'}); // 6
+store.dispatch({ type: 'DEC', payload: {}}); // 5
