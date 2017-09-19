5d54088498e78b191491196f08e69d453dcff8c2
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-19 22:22:22 +0200

    dispatching actions, error middleware, logger logging...

--- redux-middleware/src/app/index.js
+++ redux-middleware/src/app/index.js
@@ -1,16 +1,21 @@
 import { applyMiddleware, createStore } from "redux";
 
+const loggedActions = [];
+
 const initState = {
-    fetching: false
+    status: false
 };
 
 const reducer = (state = initState, action) => {
     switch(action.type) {
-        case "FETCHING":
-            state = {...state, fetching: true};
+        case "STATUS_SET":
+            state = {...state, status: true};
             break;
-        case "FETCHING_END":
-            state = {...state, fetching: false};
+        case "STATUS_UNSET":
+            state = {...state, status: false};
+            break;
+        case "THROW_ERROR":
+            throw new Error("Some error...");
             break;
     }
 
@@ -18,13 +23,28 @@ const reducer = (state = initState, action) => {
 };
 
 const logger = (store) => (next) => (action) => {
+    loggedActions.push(action);
     next(action);
 };
 
-const middleware = applyMiddleware(logger);
+const error = (store) => (next) => (action) => {
+    try {
+        next(action);
+    } catch(e) {
+        console.log(`Error occurred in: ${action.type} action`);
+    }
+};
 
-window.store = createStore(reducer, middleware);
+const middleware = applyMiddleware(logger, error);
+
+const store = createStore(reducer, middleware);
 
 store.subscribe(() => {
     console.log(store.getState());
 });
+
+store.dispatch({ type: "STATUS_SET" }); // status true
+store.dispatch({ type: "STATUS_UNSET" }); // status false
+store.dispatch({ type: "THROW_ERROR" }); // Error occurred in: THROW_ERROR action
+
+console.log(loggedActions); // [{type: "STATUS_SET"}, {type: "STATUS_UNSET"}, {type: "THROW_ERROR"}]
