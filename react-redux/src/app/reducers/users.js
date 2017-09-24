7bb9c5332d56ef53a85136b19b060a2b8959a6cf
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-24 20:54:00 +0200

    reducer update

--- react-redux/src/app/reducers/users.js
+++ react-redux/src/app/reducers/users.js
@@ -4,7 +4,26 @@ import initState from "../config/state";
 const UsersReducer = (state = initState, action) => {
     switch (action.type) {
         case ACTION_CONST.users.fetch: {
-            state = {...state, fetching: true};
+            state = {
+                ...state,
+                fetching: true
+            };
+            break;
+        }
+        case ACTION_CONST.users.fetchSuccess: {
+            state = {
+                ...state,
+                fetching: false,
+                users: action.payload
+            };
+            break;
+        }
+        case ACTION_CONST.users.fetchError: {
+            state = {
+                ...state,
+                fetching: false,
+                error: action.payload
+            };
             break;
         }
     }
