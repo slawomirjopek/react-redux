77a395234bef9b324073ca728de28ccb4ce8e032
Sławomir Jopek <pwnsoon@gmail.com>
2017-09-24 21:16:01 +0200

    userMiddleware update (fetching data by axios)

--- react-redux/src/app/middleware/user.js
+++ react-redux/src/app/middleware/user.js
@@ -1,8 +1,20 @@
 import { ACTION_CONST } from "../config/actions";
+import axios from "axios";
 
 const userMiddleware = (store) => (next) => (action) => {
     if (action.type === ACTION_CONST.users.fetch) {
-        // TODO fetch user data
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then((res) => {
+                store.dispatch({
+                    type: ACTION_CONST.users.fetchSuccess,
+                    payload: res.data
+                });
+            }, (err) => {
+                store.dispatch({
+                    type: ACTION_CONST.users.fetchError,
+                    payload: err
+                });
+            });
     }
 
     next(action);
